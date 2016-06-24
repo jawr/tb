53c2dd3ff3752d52c6202f436d75ea26dfc7d0c5
jawr <jess@lawrence.pm>
2016-06-25 00:38:02 +0200

    Add Delete and Insert methods.

--- rest/categories/categories.go
+++ rest/categories/categories.go
@@ -1,6 +1,7 @@
 package categories
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/jawr/dns/rest/paginator"
 	"github.com/jawr/dns/rest/util"
@@ -22,6 +23,18 @@ var routes = util.Routes{
 		"/{id}/",
 		ByID(serve),
 	},
+	util.Route{
+		"Insert",
+		"POST",
+		"/",
+		Insert,
+	},
+	util.Route{
+		"Delete",
+		"DELETE",
+		"/{id}",
+		Delete,
+	},
 }
 
 func Setup(router *mux.Router) {
@@ -32,6 +45,28 @@ func Setup(router *mux.Router) {
 func Search(w http.ResponseWriter, r *http.Request, params url.Values, limit, offset int) {
 	res, err := db.GetAll()
 	util.ToJSON(res, err, w)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var data db.Category
+	err := decoder.Decode(&data)
+	if err != nil {
+		util.Error(err, w)
+		return
+	}
+	obj, err := db.New(data.Name)
+	util.ToJSON(obj, err, w)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	err := db.Delete(util.ParseInt(vars["id"]))
+	if err != nil {
+		util.Error(err, w)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func ByID(fn func(http.ResponseWriter, *http.Request, []db.Category)) http.HandlerFunc {
